73f09c2236f7520ed086bd95ec1e51c78c5068bf
minhkha16 <16minhkha06@gmail.com>
2022-12-14 13:54:55 +0700

    update

--- CSHarp/QuestioMark/Program.cs
+++ CSHarp/QuestioMark/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// format code ctrl + k + d
+
+double a = 0.1;
+double b = 0.2;
+
+Console.WriteLine(a+b==0.3?"true":"false");
---
+++ CShap/Buoi2new/Program.cs
@@ -0,0 +1,11 @@
+﻿double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3 ? "true" : "false");
+decimal c = 0.1M;
+decimal d = 0.2M;
+Console.WriteLine(c+d==0.3M?"true":"false");
+// ?: => ternary operator
+
+string fullname = null;
+//int i = null;
+Nullable<int> i = null;
