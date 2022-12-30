de3ac00e121452fdf65f16f12d0876496e449d12
minhkha16 <16minhkha06@gmail.com>
2022-12-30 12:56:49 +0700

    all

---
+++ CShap/FuncAndAction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+delegate void Display();
+internal class Program
+{
+    //method
+    public static int Sub(int a, int b) => a+b;
+    public static void Show() => Console.WriteLine("kieu minh kha");
+    public static int ReturnNumber() => 5;
+    private static void Main(string[] args)
+    {
+        //local function
+        void Add(int a, int b) => Console.WriteLine(a + b);
+        //func vaf action => đều là delegate
+        //action dùng cho các phương thức là void
+        //func dùng cho phương thức khác void
+        // Display a = Show;
+        //a();  
+        //Action act = Show;
+        //Action<int, int> ac = Sub;
+
+        //act();
+        Func<int,int,int> fun = Sub ;
+        Console.WriteLine(fun(1,2));
+        Action<string> action = (str) =>
+        {
+            Console.WriteLine(str);
+        };
+        action("hehe");
+        //action.Invoke();
+    }
+}
---
+++ CShap/Tuple/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tuple;
+
+Person per = new()
+{
+    Id = 1,
+    FirstName = "kha",
+    LastName = "minh",
+};
+Console.WriteLine(per.Id);
+// dungf tuole ko cần đến class (struct)
+
+ValueTuple<int, string, string> p1 = (1, "kha", "kha");
+Console.WriteLine(p1.Item1);
+// mới hơn 
+(int, string, string) p2 = (1, "a", "a");
+// dùng var 
+var p3 = (1, "b", "b");//value tuple
+// net core dùng name thay thế item1, item2,...
+(int id , string name, string last) p5 = (1, "b", "b");
+Console.WriteLine(p5.id);
+// hoặc dùng name phía trước
+var p6 = (id: 1, firstname: "c",lastname: "a");
+Console.WriteLine(p6.firstname);
+p6.firstname = "kha";
+Console.WriteLine(p6.firstname);
+// không phải là tuple => anonymous type 
+var p7 = new{id= 1, name= "d", lastname= "kaha" };
+Console.WriteLine(p7.id);
+Console.WriteLine(p7.lastname);
+
+
+
